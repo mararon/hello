ff9fbf413d810e612284b652971b3b369525f74b
Maria Aronsson <maria.aronsson@ymail.com>
2013-06-10 21:55:56 +0200

    Update of file hello.php

--- hello.php
+++ hello.php
@@ -7,5 +7,6 @@
 <?php echo "<p>This is my first experiance with Git. I find it quite confusing!</p>" ?>
 <?php echo "<p>I'm glad to have plenty of time to learn.</p>" ?>
 <?php echo "<p>One day, perhaps I will understand.</p>" ?>
+<?php echo "<p>Anyhow, it's great fun!.</p>" ?>
 </body>
 </html>
